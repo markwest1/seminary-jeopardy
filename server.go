68b404a3e39e0c5327f65f66564a1dcf0e2e6642
Mark West <mark.west1@gmail.com>
2023-11-09 00:30:13 -0600

    server.go serves php files

--- server.go
+++ server.go
@@ -3,25 +3,56 @@ package main
 import (
 	"embed"
 	"fmt"
-	"io/fs"
 	"log"
+	"net/http"
+	"strings"
 )
 
 //go:embed data
 var content embed.FS
 
 func main() {
-	err := fs.WalkDir(content, ".", func(path string, d fs.DirEntry, err error) error {
+	http.HandleFunc("/listseasons.php", func(w http.ResponseWriter, r *http.Request) {
+		b, err := content.ReadFile("data/listseasons.php")
 		if err != nil {
-			return err
+			log.Printf("ERROR: %v", err)
 		}
-		if !d.IsDir() {
-			fmt.Printf("%s\n", path)
+
+		c, err := w.Write(b)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		} else {
+			log.Printf("%d bytes written for '%s'", c, r.Method+" "+r.Host+r.URL.String())
 		}
-		return nil
 	})
 
+	http.HandleFunc("/showseason.php", func(w http.ResponseWriter, r *http.Request) {
+		sb := strings.Builder{}
+
+		if len(r.URL.Query()) > 0 {
+			for k, v := range r.URL.Query() {
+				if strings.EqualFold(k, "season") {
+					sb.WriteString(fmt.Sprintf("%s: %s\n", k, strings.Join(v, ",")))
+				}
+			}
+		}
+
+		if len(sb.String()) == 0 {
+			sb.WriteString("No query parameters.")
+		}
+
+		c, err := w.Write([]byte(sb.String()))
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		} else {
+			log.Printf("%d bytes response to '%s'", c, r.Method+" "+r.Host+r.URL.String())
+		}
+	})
+
+	port := "8080"
+	log.Printf("Listening on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		log.Fatalf("ERROR %v", err)
+		log.Printf("ERROR: %v", err)
 	}
 }
